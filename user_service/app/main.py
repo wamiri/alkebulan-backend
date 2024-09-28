31a4f7bf16d13c28152ab7904aea856371c4fec2
Victor Momodu <victoramomodu@gmail.com>
2024-09-28 22:37:15 +0100

    refactor: Change models to schemas to stick with FastAPI convention.

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -7,7 +7,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
 
-from app.models import Token, TokenData, User, UserInDB
+from app.schemas import Token, TokenData, User, UserInDB
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
