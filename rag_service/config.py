733105d4b7c29b0c6a5b1f550493c7c5d9dee811
Victor Momodu <victoramomodu@gmail.com>
2024-09-28 18:33:13 +0100

    fix: Remove unnecessary files.

--- rag_service/config.py
+++
@@ -1,7 +0,0 @@
-from shared.environment import get_env_var
-
-
-APP_URL = get_env_var("APP_URL", "localhost:8000")
-OPENAI_API_KEY = get_env_var("OPENAI_API_KEY")
-QDRANT_API_KEY = get_env_var("QDRANT_API_KEY")
-QDRANT_API_URL = get_env_var("QDRANT_API_URL")
