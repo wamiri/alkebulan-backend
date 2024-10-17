33da363be487b22ed1ce6aab49c5ca839d7f583f
Victor Momodu <victoramomodu@gmail.com>
2024-10-17 23:40:02 +0100

    feat: Implement ingester.

--- app/ingest/dependencies.py
+++ app/ingest/dependencies.py
@@ -1,4 +1,11 @@
 import os
+import uuid
+
+from unstructured.partition.auto import partition
+from unstructured.partition.csv import partition_csv
+from unstructured.partition.doc import partition_doc
+from unstructured.partition.docx import partition_docx
+from unstructured.partition.text import partition_text
 
 from app.ingest.config import TMP_FILES_DIR
 
@@ -19,8 +26,30 @@ class TmpFilesDir:
         os.remove(f"{TMP_FILES_DIR}/{filename}")
 
 
+class Ingester:
+    def create_partition(self, filename):
+        file_type = os.path.splitext(filename)[-1].lower()
+        file_path = f"{TMP_FILES_DIR}/{filename}"
+        match file_type:
+            case ".csv":
+                return partition_csv(file_path)
+            case ".doc":
+                return partition_doc(file_path)
+            case ".docx":
+                return partition_docx(file_path)
+            case ".txt":
+                return partition_text(file_path)
+            case _:
+                return partition(file_path)
+
+
 tmp_files_dir = TmpFilesDir()
+ingester = Ingester()
 
 
 async def get_tmp_files_dir():
     return tmp_files_dir
+
+
+async def get_ingester():
+    return ingester
--- app/ingest/router.py
+++ app/ingest/router.py
@@ -4,7 +4,12 @@ from fastapi import UploadFile
 from fastapi.params import Depends
 from fastapi.routing import APIRouter
 
-from app.ingest.dependencies import TmpFilesDir, get_tmp_files_dir
+from app.ingest.dependencies import (
+    Ingester,
+    TmpFilesDir,
+    get_ingester,
+    get_tmp_files_dir,
+)
 from app.users.dependencies import get_current_user
 from app.users.models import UserData
 
@@ -14,14 +19,19 @@ router = APIRouter(prefix="/ingest", tags=["Ingest"])
 @router.post("/files/")
 async def upload_files(
     files: list[UploadFile],
-    current_user: Annotated[UserData, Depends(get_current_user)],
+    # current_user: Annotated[UserData, Depends(get_current_user)],
     tmp_files_dir: Annotated[TmpFilesDir, Depends(get_tmp_files_dir)],
+    ingester: Annotated[Ingester, Depends(get_ingester)],
 ):
-    filenames = list()
+    filenames = dict()
     for file in files:
         file_data = await file.read()
         tmp_files_dir.write_file(file.filename, file_data)
-        tmp_files_dir.delete_file(file.filename)
-        filenames.append(file.filename)
+        filenames[file.filename] = 0
+
+    for filename in filenames:
+        documents = ingester.create_partition(filename)
+        filenames[filename] = len(documents)
+        tmp_files_dir.delete_file(filename)
 
     return filenames
